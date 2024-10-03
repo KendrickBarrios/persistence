86a611fc047e597c69cca7cfe0efd40c9f7edfaa
KendrickBarrios <kkbarrios@uamv.edu.ni>
2024-10-02 21:00:55 -0600

    "Se corrigen errores relacionados a jakarta, se crean anotaciones Getter, Setter"

--- src/main/java/models/Student.java
+++ src/main/java/models/Student.java
@@ -3,9 +3,16 @@ package models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Table(name = "student")
+@Getter
+@Setter
 public class Student {
+    @Id
+    private long Id;
     private int id;
     private String name;
     private String major;
